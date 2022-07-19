1158caea36c26d90a75bfeccb20f571bd40ff6f1
sramesh <sh871216@dal.ca>
2022-07-19 16:45:43 -0400

    Add Id to model objects. closes #3

--- pet-clinic-data/src/main/java/petClinic/model/Person.java
+++ pet-clinic-data/src/main/java/petClinic/model/Person.java
@@ -1,6 +1,6 @@
 package petClinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String LastName;
 
--- pet-clinic-data/src/main/java/petClinic/model/Pet.java
+++ pet-clinic-data/src/main/java/petClinic/model/Pet.java
@@ -2,7 +2,7 @@ package petClinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private Owner owner;
     private LocalDate birthDate;
