317718fb9b419c38afd628aff6aa0c4b18b07d70
sramesh <sh871216@dal.ca>
2022-07-18 20:25:54 -0400

    adding POJO

---
+++ src/main/java/com/spring/petClinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.spring.petClinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/spring/petClinic/model/Vet.java
@@ -0,0 +1,6 @@
+package com.spring.petClinic.model;
+
+import com.spring.petClinic.model.Person;
+
+public class Vet extends Person {
+}
