f22a88143f170b8fe855f6a57dbcdd7678013ade
sramesh <sh871216@dal.ca>
2022-07-20 15:16:21 -0400

    Added index page for owner and changed owner controller to display the model with owners list

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -29,9 +29,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Prashanth");
-        owner1.setLastName("Devakumar");
+        owner2.setId(2L);
+        owner2.setFirstName("Prashanth");
+        owner2.setLastName("Devakumar");
 
         ownerService.save(owner2);
 
