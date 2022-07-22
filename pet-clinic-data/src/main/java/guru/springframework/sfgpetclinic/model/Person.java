765de5956bdc2fdf04d66fa044c9dde65b8d9bf7
sramesh <sh871216@dal.ca>
2022-07-22 17:33:02 -0400

    Covert models to JPA entities

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,8 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
+    @Column(name="first_name")
     private String firstName;
-    private String LastName;
+    @Column(name = "last_name")
+    private String lastName;
 
     public String getFirstName() {
         return firstName;
@@ -13,10 +19,10 @@ public class Person extends BaseEntity{
     }
 
     public String getLastName() {
-        return LastName;
+        return lastName;
     }
 
     public void setLastName(String lastName) {
-        LastName = lastName;
+        this.lastName = lastName;
     }
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,13 +1,30 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity
+@Table(name ="pets")
 public class Pet extends BaseEntity{
+    @Column(name ="name")
+    private String name;
 
+    @Column(name ="owner_id")
+    @ManyToOne
     private Owner owner;
+    @Column(name="birth_date")
     private LocalDate birthDate;
+    @Column
+    @ManyToOne
+    @JoinColumn(name="type_id")
     private PetType petType;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+    @ManyToOne
     public Owner getOwner() {
         return owner;
     }
@@ -30,5 +47,21 @@ public class Pet extends BaseEntity{
 
     public void setPetType(PetType petType) {
         this.petType = petType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
